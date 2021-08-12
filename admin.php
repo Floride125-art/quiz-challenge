4f992f2ef194c28f36d5c148e068a7e85ba546ea
Floride125-art <FTuyisenge120@daviscollege.com>
2021-08-12 02:45:18 +0200

    added new files for quiz challenge

---
+++ admin.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include('header.php') ?>
+        <?php 
+        session_start();
+        if(isset($_SESSION['login_id'])){
+            header('Location:home.php');
+        }
+        ?>
+		<title>Quiz Maker and Taker</title>
+	</head>
+
+	<body id='login-body' class="bg-light">
+
+        <div class=" text-center ">
+            <h3 class=" text-Black">Quiz Maker and Taker</h3>
+        </div>
+		<div class="col-md-4 offset-md-4 mt-4">
+                <div class="text-center text-black">
+                    <strong>Login</strong>
+                    <strong>For the admin login using:</strong><br>
+                    <strong>username: admin</strong><br>
+                    <strong>password: admin123</strong>
+                </div>
+            <div class="card-body">
+                     <form id="login-frm">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="username" name="username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" class="form-control">
+                        </div> 
+                        <div class="form-group text-right">
+                            <button class="btn btn-primary btn-block" style="background-color:purple" name="submit">Login</button>
+                        </div>
+                        
+                    </form>
+            </div>
+        </div>
+
+		</body>
+
+        <script>
+            $(document).ready(function(){
+                $('#login-frm').submit(function(e){
+                    e.preventDefault()
+                    $('#login-frm button').attr('disable',true)
+                    $('#login-frm button').html('Please wait...')
+
+                    $.ajax({
+                        url:'./login_auth.php?type=1',
+                        method:'POST',
+                        data:$(this).serialize(),
+                        error:err=>{
+                            console.log(err)
+                            alert('An error occured');
+                            $('#login-frm button').removeAttr('disable')
+                            $('#login-frm button').html('Login')
+                        },
+                        success:function(resp){
+                            if(resp == 1){
+                                location.replace('home.php')
+                            }else{
+                                alert("Incorrect username or password.")
+                                $('#login-frm button').removeAttr('disable')
+                                $('#login-frm button').html('Login')
+                            }
+                        }
+                    })
+
+                })
+            })
+        </script>
+</html>
