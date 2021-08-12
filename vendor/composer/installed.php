d90e042c23791b807b98afc85a085fecb5e0a63c
Floride125-art <FTuyisenge120@daviscollege.com>
2021-08-12 12:34:59 +0200

    update composer file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9e9d3c1b126cb507a9016935cf5266c5d1a3458b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9e9d3c1b126cb507a9016935cf5266c5d1a3458b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
